6476b6ce37b2ff026abc810c9058e4ab9e28af02
Foysol <foysol-chowdhury@outlook.com>
2025-06-27 20:20:39 +0100

    Descriptive message about your changes

--- variables.tf
+++ variables.tf
@@ -1,25 +1,25 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources."
   type        = string
-  default     = "eu-west-2" # CUSTOMIZE THIS: Example is London. Choose your preferred region.
+  default     = "eu-west-2" 
 }
 
 variable "organization_name" {
   description = "Name for the AWS Organization"
   type        = string
-  default     = "FoysolLandingZoneOrg" # CUSTOMIZE THIS: Choose a unique name for your organization!
+  default     = "FoysolLandingZoneOrg" 
 }
 
 variable "cloudtrail_log_bucket_base_name" {
   description = "Base name for the CloudTrail log S3 bucket (will append random string)"
   type        = string
-  default     = "foysol-lz-logs-proj-new-unique" # CUSTOMIZE THIS: Make this globally unique! (e.g., add more unique identifiers)
+  default     = "foysol-lz-logs-proj-new-unique" 
 }
 
 variable "enable_cloudtrail" {
   description = "Set to true to enable CloudTrail. Set to false to disable if experiencing errors."
   type        = bool
-  default     = true # Try to enable by default. If it causes the error, change to false.
+  default     = true 
 }
 
 variable "vpc_cidr" {
@@ -31,5 +31,5 @@ variable "vpc_cidr" {
 variable "vpc_name" {
   description = "The name tag for the VPC"
   type        = string
-  default     = "my-landing-zone-vpc" # CUSTOMIZE THIS: VPC name
+  default     = "my-landing-zone-vpc" 
 }
--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.40.0" # This means specific version 5.40.0
+      version = "5.40.0" 
     }
   }
-  required_version = ">= 1.0.0" # Ensures you're using a compatible Terraform CLI version
+  required_version = ">= 1.0.0" 
 }
